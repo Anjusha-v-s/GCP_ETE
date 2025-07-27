5300279c6d4c4cfb635d317886cb2d76b98776cf
Anjusha-v-s <anjushavs98@gmail.com>
2025-07-27 19:19:50 +0530

    first model

---
+++ target/run/GCP_ETE/models/firstmodel.sql
@@ -0,0 +1,10 @@
+
+
+  create or replace view `fine-advantage-342106`.`datatransform`.`firstmodel`
+  OPTIONS()
+  as -- models/my_first_model.sql
+
+SELECT
+  'Hello GCP!' AS greeting,
+  CURRENT_DATE() AS today;
+
